eb251598a86b6054018ea4966caf1946a25561d3
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-03-23 03:04:03 +0900

    게시글 수정권한 업데이트

--- build/resources/main/static/js/app/authority.js
+++ build/resources/main/static/js/app/authority.js
@@ -2,6 +2,14 @@ var auth={
     init:function(){
         var _this=this;
 
+        var user=$('#user').text();
+        var author=$('#author').val();
+        if(user==author){
+            $('#title').removeAttr('readonly');
+            $('#content').removeAttr('readonly');
+        }else{
+            alert(user+" "+author);
+        }
     }
 }
 auth.init();
--- src/main/resources/static/js/app/authority.js
+++ src/main/resources/static/js/app/authority.js
@@ -2,6 +2,12 @@ var auth={
     init:function(){
         var _this=this;
 
+        var user=$('#user').text();
+        var author=$('#author').val();
+        if(user==author){
+            $('#title').removeAttr('readonly');
+            $('#content').removeAttr('readonly');
+        }
     }
 }
 auth.init();
